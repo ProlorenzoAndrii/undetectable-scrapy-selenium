44bb13e476ec6d018ff7e97fce8b9915150a81a5
prolorenzo <prolorenzo.a@gmail.com>
2023-09-23 22:37:43 +0300

    fix

--- scrapy_selenium_ud/middlewares.py
+++ scrapy_selenium_ud/middlewares.py
@@ -6,7 +6,7 @@ from scrapy import signals
 from scrapy.exceptions import NotConfigured
 from scrapy.http import HtmlResponse
 import undetected_chromedriver as uc
-uc.install()
+
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from selenium.common.exceptions import TimeoutException, WebDriverException
@@ -20,61 +20,7 @@ class SeleniumUcMiddleware:
     """Scrapy middleware handling the requests using selenium"""
 
     def __init__(self, driver):
-        # self.options = options
-        # self.options.headless=True
-        # self.options.add_argument('--headless')
         self.driver = driver
-
-    # def __init__(self, driver_name, driver_executable_path,
-    #              browser_executable_path, command_executor, driver_arguments):
-    #     """Initialize the selenium webdriver
-    #     Parameters
-    #     ----------
-    #     driver_name: str
-    #         The selenium ``WebDriver`` to use
-    #     driver_executable_path: str
-    #         The path of the executable binary of the driver
-    #     driver_arguments: list
-    #         A list of arguments to initialize the driver
-    #     browser_executable_path: str
-    #         The path of the executable binary of the browser
-    #     command_executor: str
-    #         Selenium remote server endpoint
-    #     """
-    #
-    #     webdriver_base_path = f'selenium.webdriver.{driver_name}'
-    #
-    #     driver_klass_module = import_module(f'{webdriver_base_path}.webdriver')
-    #     driver_klass = getattr(driver_klass_module, 'WebDriver')
-    #
-    #     driver_options_module = import_module(f'{webdriver_base_path}.options')
-    #     driver_options_klass = getattr(driver_options_module, 'Options')
-    #
-    #     driver_options = driver_options_klass()
-    #
-    #     if browser_executable_path:
-    #         driver_options.binary_location = browser_executable_path
-    #     for argument in driver_arguments:
-    #         driver_options.add_argument(argument)
-    #
-    #     driver_kwargs = {
-    #         'executable_path': driver_executable_path,
-    #         f'{driver_name}_options': driver_options
-    #     }
-    #
-    #     # locally installed driver
-    #     if driver_executable_path is not None:
-    #         driver_kwargs = {
-    #             'executable_path': driver_executable_path,
-    #             f'{driver_name}_options': driver_options
-    #         }
-    #         self.driver = driver_klass(**driver_kwargs)
-    #     # remote driver
-    #     elif command_executor is not None:
-    #         from selenium import webdriver
-    #         capabilities = driver_options.to_capabilities()
-    #         self.driver = webdriver.Remote(command_executor=command_executor,
-    #                                        desired_capabilities=capabilities)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -82,7 +28,7 @@ class SeleniumUcMiddleware:
 
         options = ChromeOptions()
         options.add_argument('--headless')
-        options.page_load_strategy = 'none'
+        # options.page_load_strategy = 'none'
         driver = uc.Chrome(options=options)
 
         middleware = cls(
@@ -101,14 +47,6 @@ class SeleniumUcMiddleware:
             return None
 
         self.driver.get(request.url)
-
-        # for cookie_name, cookie_value in request.cookies.items():
-        #     self.driver.add_cookie(
-        #         {
-        #             'name': cookie_name,
-        #             'value': cookie_value
-        #         }
-        #     )
 
         if request.wait_until:
             WebDriverWait(self.driver, request.wait_time).until(
